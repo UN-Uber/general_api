38b419fe358f5fd9a237ae51bb6806420623849c
OscarAlejandroGomez <ogomezs@unal.edu.co>
2021-12-04 15:21:25 -0500

    Actualizacion api account

---
+++ src/schemas/creditCard/schema.js
@@ -0,0 +1,52 @@
+import { gql }  from 'apollo-server';
+
+export const creditCardTypeDefs = gql`
+
+type CreditCard{
+    idCard:Int
+    idClient:Int
+    cardNumber: String
+    dueDate: String
+    cvv: Int
+}
+
+input CreditCardInput {
+    idClient:Int!
+    cardNumber: String!
+    dueDate: String!
+    cvv: Int!
+}
+
+type Query {
+    getCards : [CreditCard]
+    getCard(idCard : Int) : CreditCard
+}
+
+type Mutation {
+    createCard(card : CreditCardInput!) : CreditCard
+    deleteCard(idCard : Int!) : Boolean
+    updateCard(idCard: Int!, card:CardInput!):CreditCard
+}
+`;
+
+export const creditCardResolvers = {
+    Query: {
+        getCards: (_source, _args, { dataSources }) => {
+            return dataSources.AccountApi.getAllCards();
+        },
+        getCard: (_source, { id }, { dataSources }) => {
+            return dataSources.AccountApi.getCardById(id);
+        },
+    },
+    Mutation: {
+        createCard: (_source, {card}, { dataSources }) => {
+            return dataSources.AccountApi.createCard(card);
+        },
+        deleteCard: (_source, {id}, { dataSources }) => {
+            return dataSources.AccountApi.deletCardById(id);
+        },
+        updateClient: (_source, {id, card}, { dataSources }) => {
+            return dataSources.AccountApi.updateCardById(id, card);
+        },
+    }
+};
---
+++ src/schemas/uclient/schema.js
@@ -0,0 +1,72 @@
+import { gql }  from 'apollo-server';
+
+export const clientTypeDefs = gql`
+type Client {
+    idClient : Int
+    fName : String
+    sName : String
+    sureName : String
+    active : Int
+    email : String
+    telNumber : String
+    password : String
+    image: String
+}
+
+type CreditCard{
+    idCard:Int
+    idClient:Int
+    cardNumber: String
+    dueDate: String
+    cvv: Int
+}
+
+input DriverInput {
+    fName : String!
+    sName : String
+    sureName : String!
+    active : Int!
+    email : String!
+    telNumber : String!
+    password : String!
+    image: String!
+}
+
+type Query {
+    getClients : [Client]
+    getClient(idClient : Int) : Client
+    getCardsClient(idClient:Int): [CreditCard]
+}
+
+type Mutation {
+    createClient(client : ClientInput!) : Client
+    deleteClient(idClient : Int!) : Boolean
+    updateClient(idClient: Int!, client:ClientInput!):Client
+}
+`;
+
+export const clientResolvers = {
+    Query: {
+        getClients: (_source, _args, { dataSources }) => {
+            return dataSources.AccountApi.getAllClients();
+        },
+        getClient: (_source, { id }, { dataSources }) => {
+            return dataSources.AccountApi.getClientById(id);
+        },
+        getCardsClient:(_source, { id }, { dataSources }) => {
+            return dataSources.AccountApi.getCardsByClient(id);
+        },
+
+    },
+    Mutation: {
+        createClient: (_source, {client}, { dataSources }) => {
+            return dataSources.AccountApi.createClient(client);
+        },
+        deleteClient: (_source, {id}, { dataSources }) => {
+            return dataSources.AccountApi.deleteClientById(id);
+        },
+        updateClient: (_source, {id, client}, { dataSources }) => {
+            return dataSources.AccountApi.updateClientById(id, client);
+        },
+    }
+};
