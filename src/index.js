16bd28a512c51ffa33e1d430a7c7443a569d2915
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2021-12-04 08:46:57 -0500

    Making changes on server: - Adding express as default server - Removing old dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ApolloServer }  from 'apollo-server';
+import { ApolloServer }  from 'apollo-server-express';
 import  _ from 'lodash';
 import { ServiceQualityApi } from './datasources/ServiceQualityApi.js';
 import {driverResolvers , driverTypeDefs} from './schemas/driver/schema.js';
@@ -6,9 +6,38 @@ import {userCTypeDefs , userCResolvers} from './schemas/userCalif/schema.js';
 import {commentTypeDefs, commentResolvers} from './schemas/comment/schema.js';
 import { communicationResolvers, communicationTypeDefs } from './schemas/communication/schema.js';
 import { CommunicationApi } from './datasources/CommunicationApi.js';
+import express from 'express';
+import http from 'http';
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 
+async function startApolloServer(){
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer({
+        typeDefs: [driverTypeDefs, communicationTypeDefs, userCTypeDefs, commentTypeDefs],
+        resolvers: _.merge(driverResolvers, communicationResolvers, userCResolvers, commentResolvers),
+        dataSources: () => {
+            return {
+                servicequalityAPI: new ServiceQualityApi(),
+                communicationAPI: new CommunicationApi()
+            };
+        },
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    });
+
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: '/',
+    })
+
+    await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+/*
 const server = new ApolloServer({
     typeDefs: [driverTypeDefs, communicationTypeDefs, userCTypeDefs, commentTypeDefs],
     resolvers: _.merge(driverResolvers, communicationResolvers, userCResolvers, commentResolvers),
@@ -24,3 +53,4 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
+*/
