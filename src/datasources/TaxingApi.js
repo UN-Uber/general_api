3fe4031a78257f205c220d4ca69e682878ef18d8
mnguzmanc <mnguzmanc@users.noreply.github.com>
2022-01-11 22:27:24 -0500

    Update TaxingApi.js

--- src/datasources/TaxingApi.js
+++ src/datasources/TaxingApi.js
@@ -8,8 +8,8 @@ export class TaxingApi extends RESTDataSource {
     }
 
     //Taxing Endpoint
-    async getPrice(startLatitude, startLongitude, endLatitude, endLongitude ){
-        const response = await axios.get(this.baseURL + '/getPrice?startLatitude=' + startLatitude + '&startLongitude=' + startLongitude + '&endLatitude=' + endLatitude + '&endLongitude=' + endLongitude);
+    async getPrice(startLatitude, startLongitude, endLatitude, endLongitude, uberType){
+        const response = await axios.get(this.baseURL + '/getPrice?startLatitude=' + startLatitude + '&startLongitude=' + startLongitude + '&endLatitude=' + endLatitude + '&endLongitude=' + endLongitude + '&uberType=' + uberType);
         return response.data;
     }
 }
