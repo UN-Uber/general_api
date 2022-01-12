001a80c831ff9fa49eddf77ba2139709c9e829e3
mnguzmanc <mnguzmanc@users.noreply.github.com>
2022-01-11 22:28:48 -0500

    Update schema.js

--- src/schemas/taxing/schema.js
+++ src/schemas/taxing/schema.js
@@ -8,17 +8,18 @@ type Taxing {
     endLatitude: Float
     endLongitude: Float
     price: String
+    uberType: Int
 }
 
 type Query {
-    getPrices(startLatitude : Float, startLongitude: Float, endLatitude: Float, endLongitude: Float) : Taxing
+    getPrices(startLatitude : Float, startLongitude: Float, endLatitude: Float, endLongitude: Float, uberType: Int) : Taxing
 }
 `;
 
 export const taxingResolvers = {
     Query: {
-        getPrices: (_source, { startLatitude , startLongitude, endLatitude, endLongitude }, { dataSources }) => {
-            let ans = dataSources.taxingApi.getPrice(startLatitude, startLongitude, endLatitude, endLongitude);;
+        getPrices: (_source, { startLatitude , startLongitude, endLatitude, endLongitude, uberType }, { dataSources }) => {
+            let ans = dataSources.taxingApi.getPrice(startLatitude, startLongitude, endLatitude, endLongitude, uberType);;
             return ans;
         }
     }
